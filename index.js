905b6ea22c7386bb81fb67974103936ff08b517b
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-15 17:58:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(katzDeliLine) {
   if(katzDeliLine.length === 0){
     return "There is nobody waiting to be served!"
   } else {
-    
+    return 
   }
   return katzDeliLine[0]
 }
