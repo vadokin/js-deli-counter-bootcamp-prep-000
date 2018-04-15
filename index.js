b51dadbb9faa321f99ef56379dc1f0bf886664e4
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-15 18:35:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ function currentLine(katzDeliLine) {
   let string = ''
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
-  }
-  for (let i = 0, len =  i < katzDeliLine.length; i < len; i++) {
-      string += katzDeliLine.length === 1 ? '' : ', ' + `${katzDeliLine[i] + 1}. ${katzDeliLine[i]}`
-      return "The line is currently: " + string   // 1. Ada, 2. Grace"
+  } else {
+    for (let i = 0, len =  i < katzDeliLine.length; i < len; i++) {
+        string += katzDeliLine.length === 1 ? '' : ', ' + `${katzDeliLine[i] + 1}. ${katzDeliLine[i]}`
+        return "The line is currently: " + string   // 1. Ada, 2. Grace"
+    }
   }
 }
