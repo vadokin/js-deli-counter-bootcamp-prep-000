990925d9425ebed0e36346a0deed3b98bda03f6c
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-15 18:52:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty."
   } else {
     for (let i = 0, len =  i < katzDeliLine.length; i < len; i++) {
-      string += `${katzDeliLine[i] + 1}. ${katzDeliLine[i]}` + (katzDeliLine.length === 1 ? '' : ', ')
+      string += `${i + 1}. ${katzDeliLine[i]}` + (katzDeliLine.length === 1 ? '' : ', ')
     }
     return "The line is currently: " + string   // 1. Ada, 2. Grace"
   }
