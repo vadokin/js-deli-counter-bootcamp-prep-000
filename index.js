ab924b455a288f09a29dee48001aa32805be0a5c
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-15 17:50:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number 1 in line.`
+  return `Welcome, ${name}. You are number ${} in line.`
 }
